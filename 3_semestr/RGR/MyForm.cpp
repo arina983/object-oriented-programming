6a775ef4fd4e52f195dffbd3fc17107897beb616
Arina <arina.bubenina@mail.ru>
2025-12-07 15:39:02 +0700

    Add files via upload

---
+++ 3_semestr/RGR/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<System::String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew RGR::MyForm());
+
+    return 0;
+}
